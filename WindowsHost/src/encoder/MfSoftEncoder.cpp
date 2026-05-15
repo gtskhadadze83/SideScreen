3eb9041167fd09c6f70b4e7bdf62f9aedd66a52f
Georhiy Tskhadadze <yurka_virus@outlook.com>
2026-05-16 01:42:04 +0200

    Update MfSoftEncoder.cpp

--- WindowsHost/src/encoder/MfSoftEncoder.cpp
+++ WindowsHost/src/encoder/MfSoftEncoder.cpp
@@ -13,6 +13,7 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
+#include <wmcodecdsp.h>
 #include <d3d11.h>
 
 // Media Foundation headers
@@ -23,7 +24,7 @@
 #include <mfreadwrite.h>
 #include <codecapi.h>
 #include <initguid.h>
-#include <wmcodecdsp.h>
+
 
 #include <cstdio>
 #include <cstring>
